f3732c7baceaa8f413cd57993491e2fbf53c21fb
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-20 23:07:19 +0500

    setup firebase messaging

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import firebase from "./firebase";
 
 function App() {
+  const messaging = firebase.messaging();
+
+  messaging
+    .requestPermission()
+    .then(() => {
+      return getToken();
+    })
+    .then((result) => console.log(result));
   return (
     <div className="App">
       <h1>PWA Push Notification</h1>
