84822d7ac96e1fa7eef3fa0d3f8889ebfe647b7b
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-20 22:36:31 +0500

    pwa-push-notification

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDqL09ZyVfcvsFaviGKnw0ez2k3YRn03fY",
+  authDomain: "pwa-push-notification-24166.firebaseapp.com",
+  databaseURL: "https://pwa-push-notification-24166.firebaseio.com",
+  projectId: "pwa-push-notification-24166",
+  storageBucket: "pwa-push-notification-24166.appspot.com",
+  messagingSenderId: "725486233878",
+  appId: "1:725486233878:web:26de147663ab723b86c837",
+};
+
+firebase.initializeApp(firebaseConfig);
